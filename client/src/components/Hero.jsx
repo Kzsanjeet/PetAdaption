f9712422e42fed8382cecaacb4dcd1c81ba05d6f
binayac <binayacharya99@gmail.com>
2024-05-06 03:47:24 +0545

    ui fixes

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <>
     <div
-    class="relative overflow-hidden bg-cover bg-no-repeat bg-[50%] h-[700px]" style={{ backgroundImage: `url(${dogandkids})` }}>
+    class="relative overflow-hidden bg-cover bg-no-repeat bg-center h-[700px]" style={{ backgroundImage: `url(${dogandkids})` }}>
     <div
       class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed bg-[hsla(0,0%,0%,0.75)]">
       <div class="flex h-full items-center justify-center">
@@ -23,13 +23,7 @@ const Hero = () => {
     </div>
   </div>
 
-  <div
-    class="-mt-2.5 text-white dark:text-neutral-800 md:-mt-4 lg:-mt-6 xl:-mt-10 h-[50px] scale-[2] origin-[top_center]">
-    <svg viewBox="0 0 2880 48" xmlns="http://www.w3.org/2000/svg">
-      <path d="M 0 48 L 1437.5 48 L 2880 48 L 2880 0 L 2160 0 C 1453.324 60.118 726.013 4.51 720 0 L 0 0 L 0 48 Z"
-        fill="currentColor"></path>
-    </svg>
-  </div>
+
     </>
   )
 }
