fa388d475bda9a4e36ddebcd634a6139a826f8dd
jangle book <np03cs4s230194@heraldcollege.edu.np>
2024-05-09 13:41:26 +0545

    icon card made responsive

--- client/src/components/IconCard.jsx
+++ client/src/components/IconCard.jsx
@@ -4,8 +4,8 @@ const IconCard = (props) => {
   return (
     <div className="flex justify-center items-center cursor-pointer">
       <div>
-        <div className="card flex flex-col justify-center py-5 px-8 bg-white rounded-lg shadow-2xl transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-200">
-          <div className="prod-img hover:border-red-500">
+        <div className="card flex flex-col justify-center py-5 px-8 bg-white rounded-lg shadow-2xl border-indigo-500 border-2 border-opacity-0 transition ease-in-out transform hover:-translate-y-1 hover:scale-110 duration-200 hover:bg-gray-200 hover:border-indigo-800 hover:border-2">
+          <div className="prod-img">
             <img src={props.icon} className="sm-6 object-cover object-center" />
           </div>
           <div className="flex flex-col md:flex-row justify-between items-center text-gray-900">
