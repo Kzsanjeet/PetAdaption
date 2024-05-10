b8f6e4cbaa4497e4b598f8228d2aa132e703574d
Kzsanjeet <sanjeetkazithapa@gmail.com>
2024-05-10 12:33:57 +0545

    update

---
+++ server/requestPet.js
@@ -0,0 +1,23 @@
+// schema for checking the status of request of the pets
+
+const mongoose = require("mongoose")
+
+const requestSchema =  new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"RegisterCustomer"
+    },
+    petId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Pet"
+    },
+    status:{
+        type:String,
+        default:"Pending"
+    }
+})
+
+
+const Request = mongoose.model("Requests", requestSchema)
+
+module.exports = Request
