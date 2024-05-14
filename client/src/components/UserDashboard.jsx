13b4af84f2901773013e587a2200f5d28577bcfc
binayac <binayacharya99@gmail.com>
2024-05-14 19:09:20 +0545

    updated main functions of the site

---
+++ client/src/components/UserDashboard.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react'
+import AddPet from './AddPet'
+import PetTable from './PetTable';
+import axios from 'axios';
+import PetList from './PetList';
+
+const UserDashboard = () => {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/getPets')
+      .then(res => setPets(res.data))
+      .catch(err => console.log(err));
+  }, []);
+  return (
+    <>
+    </>
+  )
+}
+
+export default UserDashboard
--- server/route.js
+++ server/route.js
@@ -23,7 +23,8 @@ const {registerUser,
            resetPasswordShelter,
            newPasswordShelter,
            specificShelterPets,
-           shelterData
+           shelterData,
+           showFeedback
           } = require("./control/controller");
 
 const { editUserData, deleteUserData, editShelterData, deleteShelterData } = require("./control/profile");
@@ -50,6 +51,7 @@ router.delete('/deletePet/:id', deletePet);
 router.put('/editPet/:id', editPet);
 router.get('/specific-shelter-pet/:id',specificShelterPets)
 router.route('/addFeedback/:userId').post(addFeedback)
+router.route('/showFeedback/').get(showFeedback)
 
 //route for the password reset part
 router.route('/reset-password-mail').post(resetPassword)
