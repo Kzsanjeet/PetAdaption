b84fabc0a736e5553f257c588c9f56583a9c7dd3
binay <binayacharya99@gmail.com>
2024-05-19 23:28:35 +0545

    user pet request updated

--- client/src/components/MyPet.jsx
+++ client/src/components/MyPet.jsx
@@ -1,59 +1,44 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
-const MyPet = ()=>{
-    const [request,setRequestlist]=useState([])
+const MyPet = () => {
+    const [request, setRequestlist] = useState([]);
 
-    const userId = localStorage.getItem("userId")
-    console.log(userId)
+    const userId = localStorage.getItem("userId");
 
-    const getMyPet = async()=>{
+    const getMyPet = async () => {
         try {
-            const resp = await fetch(`http://localhost:5000/get-my-booked-pet/${userId}`)
-            const data = await resp.json()
-            console.log(data)
-            if(data.success){
-                setRequestlist(data.myPets.filter((pet)=>pet.status=="Accepted"))
+            const resp = await fetch(`http://localhost:5000/get-my-booked-pet/${userId}`);
+            const data = await resp.json();
+            console.log(data);
+            if (data.success) {
+                setRequestlist(data.myPets.filter((pet) => pet.status === "Accepted"));
             }
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+    };
 
-    useEffect(()=>{
-        getMyPet()
-    },[])
-
-    console.log(request)
+    useEffect(() => {
+        getMyPet();
+    }, []);
 
     return (
         <>
-            <div className="flex justify-center">
-                <div className="w-full lg:w-3/4 p-4 lg:pl-36">
+            <h2 className= "text-center text-xl font-bold mt-20">You can visit the respective shelter houses as these pets were approved for you to adopt</h2>
+            <div className="flex justify-center mb-20">
+                <div className="w-full lg:w-3/4 p-4 mx-auto">
                     {request && request.length > 0 ? (
                         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
                             {request.map((req, index) => (
-                               
-                               <section className="text-gray-700 body-font bg-white">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="lg:w-full mx-auto flex flex-wrap">
-          <img alt={req.petId.name} className="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={`http://localhost:5000/${req.petId.image}`} />
-          <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h1>Pet details</h1>
-            <h2 className="text-sm title-font text-gray-500 tracking-widest">{req.petId.breed}</h2>
-            <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{req.petId.name}</h1>
-            <p className="leading-relaxed py-4">{req.petId.description}</p>
-
-            {/* <div className='border m-2'>
-              <h1 >Shelter Details</h1>
-              <h2 className="text-sm title-font text-gray-500 tracking-widest">{pet.shelter.firstname} {pet.shelter.lastname}</h2>
-              <h2 className="text-gray-900 text-xl title-font font-medium mb-1">{pet.shelter.sheltername}</h2>
-              <h2 className="text-gray-900 text-xl title-font font-medium mb-1">Contact: {pet.shelter.phone}</h2>
-              <p className="leading-relaxed py-4">{pet.shelter.email}</p>
-            </div> */}
-          </div>
-        </div>
-      </div>
-    </section>
+                                <div key={index} className="max-w-sm rounded overflow-hidden shadow-lg">
+                                    <div style={{height: "300px", overflow: "hidden"}}>
+                                        <img className="w-full h-full object-cover object-center" src={`http://localhost:5000/${req.petId.image}`} alt={req.petId.name} />
+                                    </div>
+                                    <div className="px-6 py-4">
+                                        <div className="font-bold text-xl mb-2">{req.petId.name}</div>
+                                        <p className="text-gray-700 text-base">{req.petId.description}</p>
+                                    </div>
+                                </div>
                             ))}
                         </div>
                     ) : (
@@ -63,7 +48,6 @@ const MyPet = ()=>{
             </div>
         </>
     );
+};
 
-}
-
-export default MyPet
+export default MyPet;
